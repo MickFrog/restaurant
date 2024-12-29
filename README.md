84ca192e7d15bc82f31eda7106bf4baf713f2b40
MickFrog <107855646+MickFrog@users.noreply.github.com>
2024-12-29 19:48:22 +0900

    check 1,2

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Restaurant
 
-Website created as part od the Odin Project.
+Website created as part of the Odin Project.
 
 ## Features
+
 1. This website uses multiple javascript modules to produce different pages
 2. It uses webpack to bundle all the different js modules into one ./dist/main.js.
 3. The styles are loaded into the website by css-loader and injected by style-loader packages.
--- src/index.js
+++ src/index.js
@@ -1,54 +1,63 @@
 import "./style.css";
 import middle from "./initial-page";
-import { headingElements, middleElements, creditElements } from "./initial-page";
+import {
+  headingElements,
+  middleElements,
+  creditElements,
+} from "./initial-page";
 import producemiddle from "./contacts";
 import produceMenu from "./menu";
 
 //create home page
-headingElements()
+headingElements();
 middleElements();
 creditElements();
 
 //Acquire elements
-let home = document.getElementById('homeBtn');
-let menu = document.getElementById('menuBtn');
-let contact = document.getElementById('contBtn');
+let home = document.getElementById("homeBtn");
+let menu = document.getElementById("menuBtn");
+let contact = document.getElementById("contBtn");
 
 //Event Listeners
-home.addEventListener('click', () => {
-    //clear middle
-    while (middle.firstChild) {
-        middle.removeChild(middle.firstChild);
-    }
+home.addEventListener("click", () => {
+  //clear middle
+  while (middle.firstChild) {
+    middle.removeChild(middle.firstChild);
+  }
 
-    middleElements();
+  middleElements();
 
-    adjustBorder(home, menu, contact);
+  adjustBorder(home, menu, contact);
 });
 
-contact.addEventListener('click', () => {
-    producemiddle(middle);
+contact.addEventListener("click", () => {
+  producemiddle(middle);
 
-    adjustBorder(contact, menu, home);
+  adjustBorder(contact, menu, home);
 });
 
-menu.addEventListener('click', () => {
-    produceMenu(middle);
+menu.addEventListener("click", () => {
+  produceMenu(middle);
 
-    adjustBorder(menu, home, contact);
+  adjustBorder(menu, home, contact);
 });
 
 function adjustBorder(target, other1, other2) {
-    target.style = 'border-bottom: .5rem solid black';
-    target.onmouseover = () => {};
+  //   target.style = "border-bottom: .5rem dotted black";
+  Object.assign(target.style, {
+    borderBottom: ".5rem dotted black",
+    color: "#CF1259",
+  });
 
-    other1.style = 'border-bottom: 0px;';
-    other1.onmouseover = () => {
-        other1.style = 'border-bottom'
-    }
+  target.onmouseover = () => {};
 
-    other2.style = 'border-bottom: 0px;';
-    other2.onmouseover = () => {
-        other2.style = 'border-bottom'
-    }
+  other1.style = "border-bottom: 0px;";
+  other1.onmouseover = () => {
+    other1.style = "border-bottom";
+  };
+
+  other2.style = "border-bottom: 0px;";
+  other2.onmouseover = () => {
+    other2.style = "border-bottom";
+  };
 }
